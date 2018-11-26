89215d8415a2a5c73f622546848d3546b0385d2e
isagri <isabelle.grillet6@orange.fr>
2018-11-26 16:36:20 +0100

    changement mois

--- src/app/home/event-detail/event-detail.component.ts
+++ src/app/home/event-detail/event-detail.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Day } from '../../models/day';
 
 @Component({
   selector: 'app-event-detail',
@@ -8,7 +7,7 @@ import { Day } from '../../models/day';
 })
 export class EventDetailComponent implements OnInit {
 
-  @Input() selectedDay: Day;
+  @Input() selectedDay: any;
 
   constructor() { }
 
--- src/app/models/event.ts
+++
@@ -1,9 +0,0 @@
-import { Datetime } from "@ionic/angular";
-
-export class Event {
-    title: string;
-    startdate: Datetime;
-    enddate: Datetime;
-    location: string;
-    description:string;
-}
